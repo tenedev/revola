b19efccd7406d67e3e47657ec767ecd8df8f26cf
Sriman <136729116+teneplaysofficial@users.noreply.github.com>
2026-05-22 15:19:57 +0530

    Update prepare.ts

--- lib/runtime/prepare.ts
+++ lib/runtime/prepare.ts
@@ -1,1 +1,9 @@
-export async function prepareRuntime() {}
+import { setEnv } from 'js-utils-kit';
+import { ctx } from './ctx';
+
+export async function prepareRuntime() {
+  setEnv('REVOLA_DATE', new Date().toISOString().split('T')[0] as string);
+  setEnv('REVOLA_NODE_PREVIOUS_VERSION', ctx.files.packageJson.content?.version || '');
+  setEnv('REVOLA_DENO_PREVIOUS_VERSION', '');
+  setEnv('REVOLA_PREVIOUS_VERSION', '');
+}
